1ee7e4ad4491b290480c66a57a41371ab5cd1d86
ltrafecanty <luke@luketraf.localdomain>
2020-11-24 23:40:28 -0800

    DHD pagination/Gensurf start

--- pull_boards.py
+++ pull_boards.py
@@ -1,28 +1,72 @@
-import requests
 import json
+import requests
+from sporting import sporting
 from config import Config as cfg
+from bs4 import BeautifulSoup
 
 def pull_pyzel():
-    response = requests.get(cfg.unofficial_endpoint_pyzel)
+    response = sporting.request(cfg.unofficial_endpoint_pyzel)
     jdata = json.loads(response.text)
     X = []
-    Y = []
     for row in jdata:
         X.append(row['img']['deck'])
-        Y.append(row['img']['bottom'])
+        X.append(row['img']['bottom'])
 
     X = list(filter(None, X))
-    Y = list(filter(None, Y))
 
     for i in range(len(X)):
-        with open("assets/board{}.jpg".format(str(i)), "wb") as f:
-            f.write(requests.get(X[i]).content)
-    j = len(X)
-    for i in range(len(Y)):
-        with open("assets/board{}.jpg".format(str(j)), "wb") as f:
-            f.write(requests.get(Y[i]).content)
-            j+=1
+        X[i] = requests.get(X[i].content)
+    image_index = save_locally(X,image_index)
+    return j
 
-    f.write(requests.get(Y[i]).content)
+def pull_gensurf(image_index,pagination=True):
+    X = []
+    page = 0
+    limit = 144
+    headers = {'limit:' str(limit), 'offset:' str(page*limit)}
+    response = requests.get(cfg.gensurf_URL,headers=headers)
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    products = html_soup.find_all('h3', class_= 'product-count').text
+    print(products)
+    # while page*limit < :
+    #     try:
+    #         response = requests.get(cfg.gensurf_URL,headers = {'limit:' str(limit), 'offset:' str(page * limit)} )
+    #     except:
+    #         break
+    #     html_soup = BeautifulSoup(response.text, 'html.parser')
+    #     for image in images:
+    #         if image is not None:
+    #             X.append(requests.get("https:{}".format(image['src'])).content)
+    #             print("https:{}".format(image['src']))
+    return None
+    # save_locally(X,image_index)
 
-pull_pyzel()
+# DHD sucks at posting product images so we will not use them
+# def pull_dhd(image_index):
+#     #DHD stores all of their products via pagination. Here we iterate through each page to get all images.
+#     X = []
+#     pages = 11
+#     for page in range(pages):
+#         if page == 0:
+#             response = requests.get(cfg.ci_surfboard_collection)
+#         elif page == 1:
+#             continue
+#         else:
+#             response = requests.get(cfg.ci_surfboard_collection+"{}".format("?page=" + str(page)))
+#         html_soup = BeautifulSoup(response.text, 'html.parser')
+#         images = html_soup.find_all('img', class_= 'product-card__image')
+#         for image in images:
+#             if image is not None:
+#                 X.append(requests.get("https:{}".format(image['src'])).content)
+#                 print("https:{}".format(image['src']))
+#     save_locally(X,image_index)
+
+def save_locally(arr,image_index):
+    for i in range(len(arr)):
+        with open("assets/board{}.jpg".format(str(image_index)), "wb") as f:
+            f.write(arr[i])
+            image_index += 1
+    return image_index + i
+#image_index  = pull_pyzel()
+image_index = 188
+image_index = pull_gensurf(image_index)
---
+++ sporting.py
@@ -0,0 +1,52 @@
+import requests
+import bs4
+from bs4 import BeautifulSoup
+import numpy as np
+import cv2
+import os
+from config import Config as cfg
+
+
+class sporting(requests.Session):
+    def __init__(self,url):
+        self.url = url
+        super().__init__()
+
+    def request(self,method,headers=None):
+        return super().request(method,self.url,headers = headers)
+
+    @staticmethod
+    def valid_board(title):
+        return True
+
+
+if __name__ == "__main__":
+    #We choose to sort by date
+    cc = sporting("https://sfbay.craigslist.org/d/sporting-goods/search/scz/sga?sort=date&")
+    response  = cc.request("GET",cfg.HEADERS)
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+
+    titles = html_soup.find_all('h3', class_= 'result-heading')
+    post_URLS = html_soup.find_all('a',class_= 'result-image gallery')
+    #
+    # for url in post_URLS:
+    #     print(url['href'], end = "")
+    #     print(url.text.replace("$",""),end = "")
+    #
+    # for title in titles:
+    #     if cc.valid_board(title.text.lower()):
+    #         print(title.text.strip())
+
+
+
+    img = cv2.imread('assets/board79.jpg')
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    fast = cv2.FastFeatureDetector_create()
+    fast.setNonmaxSuppression(False)
+
+    kp = fast.detect(gray_img, None)
+    kp_img = cv2.drawKeypoints(img, kp, None, color=(0, 255, 0))
+
+    cv2.imshow('FAST', kp_img)
+    cv2.waitKey()
